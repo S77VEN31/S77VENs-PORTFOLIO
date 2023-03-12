6597c0a1a28df633c2a5e4417357c6c250b34c0f
S77VEN <stivensg313131@outlook.com>
2023-03-12 05:02:40 -0600

    add: glitch and shake effects interval

--- src/styleguide/components/navbars/HomeNavBar/HomeNavBar.jsx
+++ src/styleguide/components/navbars/HomeNavBar/HomeNavBar.jsx
@@ -28,30 +28,46 @@ const HomeNavBar = () => {
     prevScrollPos = currentScrollPos
   }
 
+
+  setInterval(() => {
+    var elements = document.querySelectorAll(".glitch-effect");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('paused');
+      elements[i].style.animationPlayState = "running";
+    }
+    setTimeout (() => {
+      for (var i = 0; i < elements.length; i++) { 
+        elements[i].style.animationPlayState = "paused";
+        elements[i].classList.add('paused');
+      }
+    }, 1000);
+  }, 6000);
+  
+  
   return (
     <div>
       <header className='navbar-main-container'>
-        <img
-          style={{ maxWidth: 100 }}
-          src={TitleDescriptionLogo}
-          className='testxd'
-        />
+        <div className='my-image'>
+          <img
+            style={{ maxWidth: 100 }}
+            src={TitleDescriptionLogo}
+            className='shake'
+          />
+        </div>
         <ol className='navbar-routes-container'>
-          <li className='Inter-Black-H2 route-container text-hover glitch-effect'>
-            <span aria-hidden="true">Home</span>
+          <li className='Inter-Black-H2 route-container text-hover glitch-effect paused'>
             Home
-            <span aria-hidden="true">Home</span>
           </li>
-          <li className='Inter-Black-H2 route-container text-hover glitch-effect'>
+          <li className='Inter-Black-H2 route-container text-hover glitch-effect paused'>
             About
           </li>
-          <li className='Inter-Black-H2 route-container text-hover glitch-effect'>
+          <li className='Inter-Black-H2 route-container text-hover glitch-effect paused'>
             Contact
           </li>
-          <li className='Inter-Black-H2 route-container text-hover glitch-effect'>
+          <li className='Inter-Black-H2 route-container text-hover glitch-effect paused'>
             Testimonials
           </li>
-          <li className='Inter-Black-H2 route-container text-hover glitch-effect'>
+          <li className='Inter-Black-H2 route-container text-hover glitch-effect paused'>
             Updates
           </li>
         </ol>
