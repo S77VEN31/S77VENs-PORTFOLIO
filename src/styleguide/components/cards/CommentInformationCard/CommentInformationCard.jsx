4fc0dceee3a229515cf4db0727c6841b8b8e6b2c
S77VEN <stivensg313131@outlook.com>
2023-03-14 21:40:45 -0600

    add: example card

---
+++ src/styleguide/components/cards/CommentInformationCard/CommentInformationCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CommentInformationCard.css'
+const CommentInformationCard = () => {
+  return (
+    <div className='main-container-xd' />
+  )
+}
+export default CommentInformationCard
