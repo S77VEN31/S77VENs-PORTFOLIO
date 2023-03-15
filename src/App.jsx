7e953da7fab4a476bf2c73c912f7ca9a6f5fc34d
S77VEN <stivensg313131@outlook.com>
2023-03-15 17:26:30 -0600

    Hotfix: window height bug fix Add: scroll content behavior

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,42 @@ import HomeNavBar from './styleguide/components/navbars/HomeNavBar/HomeNavBar'
 import './App.css'
 import CommentInformationCard from './styleguide/components/cards/CommentInformationCard/CommentInformationCard'
 const App = () => {
-  let prevScrollPos = window.pageYOffset
-
-  window.onscroll = () => {
-    const currentScrollPos = window.pageYOffset
-    const mainHeader = document.querySelector('.app-main-content')
-
-    if (prevScrollPos > currentScrollPos) {
-      mainHeader.classList.remove('hide')
-    } else {
-      mainHeader.classList.add('hide')
-    }
-
-    prevScrollPos = currentScrollPos
-  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.app-main-content')
+    let prevScrollPos = container.scrollTop
+    container.addEventListener('scroll', () => {
+      const currentScrollPos = container.scrollTop
+      const mainHeader = document.querySelector('.navbar-main-container')
+      const mainContent = document.querySelector('.app-main-content')
+      if (currentScrollPos > 0) {
+        mainHeader.classList.remove('box-shadow-none')
+        mainHeader.classList.add('box-shadow')
+      } else {
+        mainHeader.classList.remove('box-shadow')
+        mainHeader.classList.add('box-shadow-none')
+      }
+      if (prevScrollPos > currentScrollPos) {
+        mainHeader.classList.remove('hide')
+        mainHeader.classList.add('show')
+        mainContent.classList.add('margin')
+      } else {
+        mainHeader.classList.add('hide')
+        mainHeader.classList.remove('show')
+        mainContent.classList.remove('margin')
+      }
+      prevScrollPos = currentScrollPos
+    })
+  })
   return (
     <div className='app-main-container '>
       <HomeNavBar />
       <main className='app-main-content'>
-        <CommentInformationCard />
+        {
+          Array.from({ length: 1000 }, (_, index) => (
+            <CommentInformationCard key={index} comment={index} />
+          ))
+        }
       </main>
-
     </div>
   )
 }
